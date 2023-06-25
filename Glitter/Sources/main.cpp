90e14f5185b45fe76ce2572f4b02a8a479e852c5
legit-null <68524238+legit-null@users.noreply.github.com>
2023-06-25 09:23:51 +0800

    feat: hello window

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -8,41 +8,69 @@
 // Standard Headers
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    std::cout << "callback called" << std::endl;
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(300, 400, width, height);
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
 
 int main(int argc, char * argv[]) {
 
     // Load GLFW and Create a Window
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
     auto mWindow = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
 
     // Check for Valid Context
-    if (mWindow == nullptr) {
+    if (!mWindow) {
         fprintf(stderr, "Failed to Create OpenGL Context");
+        glfwTerminate();
         return EXIT_FAILURE;
     }
 
     // Create Context and Load OpenGL Functions
     glfwMakeContextCurrent(mWindow);
+    // glfwSetFramebufferSizeCallback(mWindow, framebuffer_size_callback);
+
     gladLoadGL();
     fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
 
+    // TODO: Does not work?
+    glViewport(200, 400, 800, 600);
+
     // Rendering Loop
-    while (glfwWindowShouldClose(mWindow) == false) {
-        if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(mWindow, true);
+    while (!glfwWindowShouldClose(mWindow)) {
+        processInput(mWindow);
 
         // Background Fill Color
-        glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+        glClearColor(1.f, 0.25f, 0.25f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Flip Buffers and Draw
         glfwSwapBuffers(mWindow);
         glfwPollEvents();
-    }   glfwTerminate();
+    }   
+
+    glfwDestroyWindow(mWindow);
+    glfwTerminate();
+
     return EXIT_SUCCESS;
 }
